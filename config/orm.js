ca57ba69dc7577c1fc0455e536dc99c277bd1adf
Marco Alfaro <marco.alfaro@gmail.com>
2016-08-17 18:05:29 -0600

    'adjustments'

---
+++ config/orm.js
@@ -0,0 +1,8 @@
+// config/orm.js
+module.exports.orm = {
+  _hookTimeout: 60000 // I used 60 seconds as my new timeout
+};
+
+module.exports.pubsub = {
+  _hookTimeout: 60000 // I used 60 seconds as my new timeout
+};
